22be76f071723deeaa3aad98f0f2d94fc2461b06
Andaru Hymawan <130301294+AndaruHP@users.noreply.github.com>
2026-08-16 22:50:55 +0700

    add jaeger for tracing

--- services/payment-service/src/main/java/com/ecommerce/paymentservice/config/RestClientConfig.java
+++ services/payment-service/src/main/java/com/ecommerce/paymentservice/config/RestClientConfig.java
@@ -1,6 +1,7 @@
 package com.ecommerce.paymentservice.config;
 
 import com.ecommerce.securitycommon.ServiceTokenProvider;
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -8,12 +9,13 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
     @Bean
-    public RestClient restClient(ServiceTokenProvider serviceTokenProvider) {
+    public RestClient restClient(ObservationRegistry observationRegistry, ServiceTokenProvider serviceTokenProvider) {
         return RestClient.builder()
                 .requestInterceptor((request, body, execution) -> {
                     request.getHeaders().setBearerAuth(serviceTokenProvider.getToken());
                     return execution.execute(request, body);
                 })
+                .observationRegistry(observationRegistry)
                 .build();
     }
 }
